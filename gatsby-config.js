cf470e4a9cdfbce0df21cfa6e4e8f42e80cc3a91
Jonathan Bryce <jbryce@jbryce.com>
2020-07-08 09:56:58 -0500

    Adding in GA code for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "",
+        trackingId: "UA-172047994-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
