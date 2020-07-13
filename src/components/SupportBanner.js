c9f04da1aff0b58df2bfb7e5f0fea89244e282b7
Wes Wilson <wes@iamweswilson.com>
2020-07-13 10:56:36 -0700

    Added OAI and TIP logos as supporters

--- src/components/SupportBanner.js
+++ src/components/SupportBanner.js
@@ -2,7 +2,8 @@ import React from "react";
 import content from "../content/footer-nav.json";
 
 import OSFLogo from "../../static/img/osf-logo.svg";
-import SupporterLogo from "../../static/img/supporter-logo-placeholder.png";
+import TIPLogo from "../../static/img/TIP_horizontal_rgb_color.png";
+import OAILogo from "../../static/img/oai_final_logo.png";
 
 const SupportBanner = class extends React.Component {
   constructor(props) {
@@ -19,29 +20,19 @@ const SupportBanner = class extends React.Component {
               {content.productName} is an open source project supported by
             </h4>
             <div className="columns is-multiline">
-              <div className="column is-one-fifth">
-                <a href="//facebook.com">
-                  <img src={SupporterLogo} width="250px" alt="OSF logo" />
+              <div className="column is-one-third vert-centered">
+                <a href="//www.openairinterface.org">
+                  <img src={OAILogo} width="250px" alt="OAI logo" />
                 </a>
               </div>
-              <div className="column is-one-fifth">
-                <a href="//facebook.com">
-                  <img src={SupporterLogo} width="250px" alt="OSF logo" />
-                </a>
-              </div>
-              <div className="column is-one-fifth">
-                <a href="//facebook.com">
-                  <img src={SupporterLogo} width="250px" alt="OSF logo" />
-                </a>
-              </div>
-              <div className="column is-one-fifth">
-                <a href="//facebook.com">
-                  <img src={SupporterLogo} width="250px" alt="OSF logo" />
-                </a>
-              </div>
-              <div className="column is-one-fifth">
+              <div className="column is-one-third vert-centered">
                 <a href="//osf.dev">
                   <img src={OSFLogo} width="250px" alt="OSF logo" />
+                </a>
+              </div>
+              <div className="column is-one-third vert-centered">
+                <a href="https://telecominfraproject.com">
+                  <img src={TIPLogo} width="250px" alt="OSF logo" />
                 </a>
               </div>
             </div>
