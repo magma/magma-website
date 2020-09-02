44922ac692f2b36c11bb23b366c7fcffe858c994
Jimmy McArthur <jimmy@tipit.net>
2020-09-02 17:20:23 -0500

    Add Magma logo to top nav
    - Created new navbar logo position
    - Add supporting styles for nav-brand
    
    Signed-off-by: Jimmy McArthur <jimmy@tipit.net>

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,6 +3,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "/style/homepage.css";
 
 const Reviews = class extends React.Component {
   render() {
