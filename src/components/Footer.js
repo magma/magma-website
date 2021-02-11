3aa848201eeb983d9acf80c2073a55122ade4359
Phillip Ritter <pritter@fb.com>
2021-02-11 14:33:55 -0800

    Fix typo in footer
    Signed-off-by: Phillip Ritter <pritter@fb.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -54,13 +54,13 @@ const Footer = class extends React.Component {
                   <div className="footer-entry">
                     <p>
                       &copy; 2021 Magma Core. Copyright &copy; 2021 Magma Core and Magma are trademarks of LF Projects LLC.
-                      For trademark usage guidelines, terms of use, privacy policy and the code of conduct, please see\ 
+                      For trademark usage guidelines, terms of use, privacy policy and the code of conduct, please see
                       <OutboundLink
                         href="https://www.lfprojects.org/policies/"
                         target="_blank"
                         rel="noopener noreferrer"
                       >
-                        LF Projects Policies
+                        " LF Projects Policies"
                       </OutboundLink>
                       .
                     </p>
