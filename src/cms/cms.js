5f5eb81a4550ebd8d8833976767d0bb0bc193517
Tomás Castillo <tcastilloboireau@gmail.com>
2021-07-13 14:31:59 -0300

    Changes in previews for CMS
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,11 @@
 import CMS from 'netlify-cms-app'
 
-/*import DefaultPagePreview from './preview-templates/DefaultPagePreview'
+import DefaultPagePreview from './preview-templates/DefaultPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import SupportersPagePreview from './preview-templates/SupportersPagePreview'
 import CaseStudiesPagePreview from './preview-templates/CaseStudiesPagePreview'
-import CommitteePagePreview from './preview-templates/CommitteePagePreview'*/
+import CommitteePagePreview from './preview-templates/CommitteePagePreview'
 
 import { Widget as FileRelationWidget } from '@ncwidgets/file-relation'
 import { Widget as IdWidget } from '@ncwidgets/id'
@@ -13,7 +13,7 @@ import { Widget as IdWidget } from '@ncwidgets/id'
 CMS.registerWidget(IdWidget)
 CMS.registerWidget(FileRelationWidget)
 
-/*CMS.registerPreviewStyle('style/styles.scss');
+CMS.registerPreviewStyle('style/styles.scss');
 
 CMS.registerPreviewTemplate('indexPage', IndexPagePreview)
 CMS.registerPreviewTemplate('pages', DefaultPagePreview)
@@ -23,4 +23,4 @@ CMS.registerPreviewTemplate('caseStudiesPage', CaseStudiesPagePreview)
 CMS.registerPreviewTemplate('tac', CommitteePagePreview)
 CMS.registerPreviewTemplate('tsc', CommitteePagePreview)
 CMS.registerPreviewTemplate('governingBoard', CommitteePagePreview)
-CMS.registerPreviewTemplate('outreachCommittees', CommitteePagePreview)*/
+CMS.registerPreviewTemplate('outreachCommittees', CommitteePagePreview)
