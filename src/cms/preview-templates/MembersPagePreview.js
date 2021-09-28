ecc62b61d38797529d03944583735fd3cf6fcc93
Tomás Castillo <tcastilloboireau@gmail.com>
2021-09-28 20:03:09 -0300

    Preview for images uploaded in CMS
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/cms/preview-templates/MembersPagePreview.js
+++ src/cms/preview-templates/MembersPagePreview.js
@@ -4,18 +4,35 @@ import { MembersPageTemplate } from '../../templates/members-page'
 
 const MembersPagePreview = ({ entry, getAsset, widgetFor }) => {
     const data = entry.getIn(['data']).toJS();
-    
-    const entryMembers = entry.getIn(['data', 'members']);
-    const members = entryMembers ? entryMembers.toJS() : [];
 
-    return (
-        <MembersPageTemplate
-            title={entry.getIn(['data', 'title'])}
-            subTitle={entry.getIn(['data', 'subTitle'])}
-            members={members}
-            cta={data.cta || {}}
-        />
-    )
+    const entryMembers = entry.getIn(['data', 'members'])
+    const members = entryMembers ? entryMembers.toJS() : []
+
+    const membersArray = members.map((member) => {
+        return {
+            category: member.category,
+            membersList: member.membersList.map((memberItem) => {
+                return {
+                    name: memberItem.name,
+                    logo: getAsset(memberItem.logo),
+                    url: memberItem.url,
+                }
+            })
+        }
+    })
+
+    if (data && membersArray) {
+        return (
+            <MembersPageTemplate
+                title={entry.getIn(['data', 'title'])}
+                subTitle={entry.getIn(['data', 'subTitle'])}
+                members={membersArray}
+                cta={data.cta || {}}
+            />
+        )
+    } else {
+        return <div>Loading...</div>
+    }
 }
 
 MembersPagePreview.propTypes = {
